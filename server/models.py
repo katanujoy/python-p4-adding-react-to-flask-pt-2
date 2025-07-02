f3eb391688e99b0ffe32c5c28d8d67c756ae1af2
katanujoy <katanujoyy99@gmail.com>
2025-07-02 09:14:37 +0300

    added-react -to-flask-pt-2

--- server/models.py
+++ server/models.py
@@ -1,13 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Movie(db.Model, SerializerMixin):
+class Movie(db.Model):
     __tablename__ = 'movies'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
+    genre = db.Column(db.String)
+    year = db.Column(db.Integer)
 
-    def __repr__(self):
-        return f'<Movie {self.title}>'
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "genre": self.genre,
+            "year": self.year
+        }
